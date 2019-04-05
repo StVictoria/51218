b795ef26daed4f472b6a0b55f10a22d05953e213
StVictoria <2018vvs@gmail.com>
2019-04-05 10:56:56 +0300

    Initial commit

---
+++ catalog.php
@@ -0,0 +1,30 @@
+<?php
+    $pageConfig = [
+        'title' => 'Каталог',
+        'cssFiles' => [
+            '/css/style.css',
+            '/css/catalog.css'
+        ],
+        'jsFiles' => [
+            '/js/script.js',
+            '/js/catalog.js'
+        ],
+    ];
+
+    $template = [
+        'section' => (isset($_GET['section'])) ? $_GET['section'] : 'man'
+    ];
+
+    include($_SERVER['DOCUMENT_ROOT'].'/parts/header.php');
+?>
+
+    <div class="catalog" data-section='<?=$template['section']?>'>
+        <div class="catalog_filters"></div>
+        <div class="catalog_products"></div>
+        <div class="catalog_pagination"></div>
+
+    </div>
+
+<?php
+    include($_SERVER['DOCUMENT_ROOT'].'/parts/footer.php'); 
+?>
---
+++ js/catalog.js
@@ -0,0 +1,150 @@
+class Product{
+    constructor(name, price, img_src, id){
+        this.name = name; //1nmae - свойство, 2name - название
+        this.price = price;
+        this.img_src = img_src;
+        this.id = id;
+    }
+    show(){
+        alert(`${this.name} ${this.price} ${this.img_src}`);
+    }
+    render(parentEl){
+        let productItem = document.createElement('a');
+        productItem.href = `/product.php?id=${this.id}`;
+        productItem.classList.add('catalog_products_item');
+        productItem.innerHTML = `
+            <img src='${this.img_src}'>
+            <h2>${this.name}</h2>
+            <p>${this.price} руб.</p>
+        `;
+        parentEl.appendChild(productItem);
+    } 
+}
+
+class Catalog{
+    constructor(){
+        this.el = document.querySelector('.catalog');
+        this.section = this.el.getAttribute('data-section');
+        this.products = [];
+        this.filters = {
+            price:[
+                [0, 1000],
+                [1000, 2000],
+                [2000, 3500],
+                [3500, 5000],
+                [5000, 10000],
+                [10000, 20000]
+            ],
+            activePrice: null
+        };
+        this.renderFilterPrice();
+    }
+    addProducts(productsArray){
+        productsArray.forEach((product) => {
+            this.products.push(product);
+        });
+    }
+    renderFilterPrice(){
+        let select = document.createElement('select');
+        select.name = 'filter_price';
+        this.filters.price.forEach((priceArr) =>{
+            let option = document.createElement('option');
+            option.innerHTML = `${priceArr[0]}-${priceArr[1]} руб.`;
+            option.value = `${priceArr[0]}-${priceArr[1]} руб.`;
+
+            select.appendChild(option);
+       });
+       let that = this;
+
+       select.addEventListener('change', function(){
+           that.filters.activePrice = this.value;
+           that.load();
+       })
+
+        this.el.querySelector('.catalog_filters').appendChild(select);
+    }
+    render(){
+        let catalogProductsBox = this.el.querySelector('.catalog_products');
+        catalogProductsBox.innerHTML = [];
+
+        this.products.forEach((product) => {
+            product.render(catalogProductsBox);
+        });
+    }
+    preloadOn(){
+        this.el.classList.add('preload');
+    }
+    preloadOff(){
+        this.el.classList.remove('preload');
+    }
+    paginationRender(paginationConfig){
+        let paginationEl = this.el.querySelector('.catalog_pagination');
+        paginationEl.innerHTML = '';
+
+        for (let i=1; i<=paginationConfig.countPage; i++){
+            let div = document.createElement('div');
+            div.classList.add('catalog_pagination_item');
+
+            if (i == paginationConfig.nowPage){
+                div.classList.add('active');
+            }
+
+            div.innerHTML = i;
+            div.setAttribute('data_page_id', i);
+
+            let that = this;
+            div.addEventListener('click', function(){
+                let pageNum = this.getAttribute('data_page_id');
+                that.load(pageNum);
+            });
+
+            paginationEl.appendChild(div);
+        }
+    }
+    load(page = 1){
+        this.preloadOn();
+        let xhr = new XMLHttpRequest();
+        let path = `/handlers/catalog_handler.php?page=${page}&section=${this.section}`;
+        if (this.filters.activePrice != null){
+            path += `&filter_price=${this.filters.activePrice}`;
+        }
+        xhr.open('GET', `/handlers/catalog_handler.php?page=${page}&section=${this.section}`);
+        xhr.send();
+        xhr.addEventListener('load', () => {
+            let data = JSON.parse(xhr.responseText);
+            this.paginationRender(data.pagination);
+            this.products = [];
+            data.products.forEach((productItem)=>{
+              this.products.push( new Product (productItem.name, productItem.price, productItem.img_src, productItem.id) );  
+            });
+            this.render();
+            this.preloadOff();
+        });
+    }
+}
+
+let catalog = new Catalog();
+// catalog.addProducts([
+//     new Product('Кроссовки', 11000, '/images/1.png', 1),
+//     new Product('Футболка', 3000, '/images/2.png', 2)
+// ]);
+catalog.load();
+
+// setTimeout(() =>{
+//     catalog.preloadOn();
+// }, 1000);
+// setTimeout(() =>{
+//     catalog.preloadOff();
+// }, 2000);
+
+// let boots = new Product('Кроссовки', 11000, '/images/1.png', 1);
+// let tshirts = new Product('Футболка', 3000, '/images/2.png', 2);
+// console.log(boots);
+// console.log(tshirts);
+
+// // boots.show();
+// // tshirts.show();
+
+// let catalogProductsBox = document.querySelector('.catalog_products');
+// boots.render(catalogProductsBox);
+// tshirts.render(catalogProductsBox);
