77e191a78ac82b175fc58a8d550d9ee616b400a1
StVictoria <2018vvs@gmail.com>
2019-04-05 11:36:59 +0300

    Добавлен div

--- parts/footer.php
+++ parts/footer.php
@@ -29,16 +29,7 @@
                     <div class="instagram"><i class="fab fa-instagram"></i></div>
                 </div>
             </div>
-            <div class="footer_block new">
-                <h3>Коллекции</h3>
-                <div class="list">
-                    <a href="#">Женщинам</a>
-                    <a href="#">Мужчинам</a>
-                    <a href="#">Детям</a>
-                    <a href="#">Новинки</a>
-                </div>
-            </div>
-        </div>
+            <div class="skjdaskj"></div>
 </div>
     <?php foreach($pageConfig['jsFiles'] as $path_js): ?>
         <script src='<?=$path_js?>'></script>
